99cb3785e66cceb92909b318e9273a0a5cb733a3
Arindam <arindamsanyal.2013@gmail.com>
2017-10-08 19:49:47 +0530

    Committing ACE core java learning changes

---
+++ ACE JAVA/src/com/test/custom/concurrentcounter/MainTest.java
@@ -0,0 +1,16 @@
+package com.test.custom.concurrentcounter;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		AtomicCounter counter = new AtomicCounter();
+		for (int i = 0; i < 20; i++) {
+			IncrementingThread increThread = new IncrementingThread(counter);
+			DecrementingThread decreThread = new DecrementingThread(counter);
+			increThread.start();
+			decreThread.start();
+		}
+
+	}
+
+}
